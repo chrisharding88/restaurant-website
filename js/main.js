ae7eaa864606262e62f88a1d89c7f18e55896622
Christopher Harding <chrisharding88@gmail.com>
2017-06-11 00:26:32 -0400

    added new javascript for the slideshow

--- js/main.js
+++ js/main.js
@@ -3,24 +3,74 @@ $(document).ready(function(){
 /*********************************
 Slideshow for the menu
 *********************************/
-	   var count = 0;
-	   var images = ["../img/burritos.jpg","../img/chips-and-guac.jpg","../img/sopa-de-frioles.jpg","../img/tacos.jpg", "../img/the-perfect-flan.jpg"];
-	   var time = 5000;
+	  function Slideshow(element){
+	  		this.el = document.querySelector(element);
+	  		this.init();
+	  }
 
-	   function changeImg(){
-	   		document.getElementById("img-slideshow").src = images[count];
+	  Slideshow.prototype = {
 
-	   		if (count < images.length - 1){
-	   			count++;
-	   		}else{
-	   			count = 0;
-	   		}
+	  	init: function() {
+	  		this.wrapper = this.el.querySelector(".slider-wrapper")
+	  		this.slides = this.el.querySelectorAll(".slides")
+	  		this.previous = this.el.querySelectorAll(".slider-previous")
+	  		this.next= this.el.querySelector(".slider-next")
+	  		this.index = 0
+	  		this.total = this.slides.length;
+	  		this.timer = null;
 
-	   		setTimeout("changeImg()", time);
 
-	   }
+	  		this.action();
+	  		this.stopStart();
 
-	   window.onload = changeImg;
+	  	},
+
+	  	_slideTo: function(slide) {
+	  		var currentSlide = this.slides[slide];
+	  		currentSlide.styles.opacity = 1;
+	  	}
+
+	  	for(var i = 0; i<this.slides.length; i++){
+	  		var slide = this.slides[i];
+
+	  		if(slide !== currentSlide){
+	  			slide.style.opacity = 0;
+	  		}
+	  	}
+
+	  	action: function{
+	  		var self = this;
+	  		self.timer = setInterval(function(){
+	  			self.index++;
+	  			if(self.index == self.slides.length){
+	  				self.index = 0;
+	  			}
+	  			self._slideTo(self.index);
+	  		}, 3000)
+	  	},
+
+	  	stopStart: function(){
+	  		var self = this;
+	  		self.el.addEventListener("mouseover", function(){
+	  			clearInterval(self.timer);
+	  			self.timer = null
+	  		}, false);
+
+	  		self.el.addEventListener("mouseout", function(){
+	  			self.action()
+	  		}, false)
+	  	}
+
+
+	  	document.addEventListener("DOMContentLoaded, function(){
+
+	  		var slider = new Slideshow(".slider")
+	  	})
+
+
+
+	  }
+	   
 
 
 
